964e02a67bc4c4f15b80e3661c52ebd6547a0b80
pipluppers <hadoop5@th-01.cs.ucr.edu>
2019-03-05 21:28:41 -0800

    Added comments explaining functions

--- WordCount.java
+++ WordCount.java
@@ -12,10 +12,21 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
 
+    // Mapper< KEYIN, VALUEIN, KEYOUT, VALUEOUT >
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
+
+	// Create an IntWritable (basically an integer) with the value 1
+	private final static IntWritable one = new IntWritable(1);
+
         private Text word = new Text();
+
+	// Called once for each key-value pair in the input split
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		
+		// StringTokenizer takes in a string (e.g. value.toString)
+		// Each token is a word (space-separated)
+		// Basically, using StringTokenizer and nextToken() works like the split function with strings in 
+		// 	the regex library
             StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
@@ -26,12 +37,21 @@ public class WordCount {
 
     public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
         private IntWritable result = new IntWritable();
+
+	// Grabs the key and all of the values associated with that key
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
+
+		// Loops through all of the values
             for (IntWritable val : values) {
+		// Use val.get() to get the value (i.e. integer) of an IntWritable
                 sum += val.get();
             }
+
+		// Assign the integer sum to the IntWritable result
             result.set(sum);
+
+		// Emit the key-value pair
             context.write(key, result);
         }
     }
